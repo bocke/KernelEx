07556784b407a812180bf7ce9a28241ec8362f57
xeno86 <xeno86@3ad93e46-0ec0-498d-9331-a2525978a03b>
2009-05-21 08:15:44 +0000

    accept CP_THREAD_ACP flag for MultiByteToWideChar and WideCharToMultiByte

---
+++ apilibs/kexbases/Kernel32/widecharconv.c
@@ -0,0 +1,60 @@
+/*
+ *  KernelEx
+ *  Copyright (C) 2009, Xeno86
+ *
+ *  This file is part of KernelEx source code.
+ *
+ *  KernelEx is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published
+ *  by the Free Software Foundation; version 2 of the License.
+ *
+ *  KernelEx is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with GNU Make; see the file COPYING.  If not, write to
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+
+#include <windows.h>
+
+#ifndef CP_THREAD_ACP
+#define CP_THREAD_ACP 3
+#endif
+
+/* MAKE_EXPORT WideCharToMultiByte_new=WideCharToMultiByte */
+int WINAPI WideCharToMultiByte_new(
+    UINT CodePage,
+    DWORD dwFlags,
+    LPCWSTR lpWideCharStr,
+    int cchWideChar,
+    LPSTR lpMultiByteStr,
+    int cchMultiByte,
+    LPCSTR lpDefaultChar,
+    LPBOOL lpUsedDefaultChar
+   )
+{
+	if (CodePage == CP_THREAD_ACP)
+		CodePage = CP_ACP;
+	return WideCharToMultiByte(CodePage, dwFlags, lpWideCharStr, cchWideChar, 
+		lpMultiByteStr, cchMultiByte, lpDefaultChar, lpUsedDefaultChar);
+}
+
+/* MAKE_EXPORT MultiByteToWideChar_new=MultiByteToWideChar */
+int WINAPI MultiByteToWideChar_new(
+    UINT CodePage,
+    DWORD dwFlags,
+    LPCSTR lpMultiByteStr,
+    int cchMultiByte,
+    LPWSTR lpWideCharStr,
+    int cchWideChar
+   )
+{
+	if (CodePage == CP_THREAD_ACP)
+		CodePage = CP_ACP;
+	return MultiByteToWideChar(CodePage, dwFlags, lpMultiByteStr, cchMultiByte,
+		lpWideCharStr, cchWideChar);
+}
