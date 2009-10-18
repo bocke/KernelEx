e4228e9cda980bbe45ebf3d4f5dda4e386379417
xeno86 <xeno86@3ad93e46-0ec0-498d-9331-a2525978a03b>
2009-10-18 17:10:27 +0000

    fixed uxtheme return addresses implemented theme metric functions

--- trunk/auxiliary/uxtheme/uxtheme.c
+++ trunk/auxiliary/uxtheme/uxtheme.c
@@ -1,6 +1,6 @@
 /*
  *  KernelEx
- *  Copyright (C) 2008, Xeno86
+ *  Copyright (C) 2008-2009, Xeno86
  *
  *  This file is part of KernelEx source code.
  *
@@ -21,86 +21,60 @@
 
 #include <windows.h>
 
-int WINAPI CommonUnimpStub(void);
-
-#ifdef __GNUC__
-#define UNIMPL_FUNC(name,params) \
-	__asm__( ".text\n" \
-			 ".globl _" #name "@0\n" \
-			 "_" #name "_new@0:\n\t" \
-			 "xor %eax, %eax\n\t" \
-			 "movb $" #params ", %cl\n\t" \
-			 "jmp _CommonUnimpStub@0\n\t" \
-	)
-#else
-#define UNIMPL_FUNC(name,params) \
-	int __declspec(naked) __stdcall name() \
+#define UNIMPL_FUNC(name,params,val) \
+	unsigned long __declspec(naked) __stdcall name() \
 	{ \
-		__asm xor eax,eax \
-		__asm mov cl, params \
-		__asm jmp CommonUnimpStub \
+		__asm mov eax, val \
+		__asm ret 4*params \
 	}
-#endif
+
+#define _E_NOTIMPL 0x80004001
 
 BOOL WINAPI DllMain (HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
 {
     if (fdwReason == DLL_PROCESS_ATTACH)
-    {
-		OSVERSIONINFO osv;
-		osv.dwOSVersionInfoSize = sizeof(OSVERSIONINFO);
-		GetVersionEx(&osv);
-		if (osv.dwMajorVersion < 5 || (osv.dwMajorVersion == 5 && osv.dwMinorVersion == 0))
-			return FALSE;
 		DisableThreadLibraryCalls(hinstDLL);
-    }
     return TRUE;
 }
 
-UNIMPL_FUNC(CloseThemeData, 1);
-UNIMPL_FUNC(DrawThemeBackground, 6);
-UNIMPL_FUNC(DrawThemeBackgroundEx, 6);
-UNIMPL_FUNC(DrawThemeEdge, 8);
-UNIMPL_FUNC(DrawThemeIcon, 7);
-UNIMPL_FUNC(DrawThemeParentBackground, 3);
-UNIMPL_FUNC(DrawThemeText, 9);
-UNIMPL_FUNC(EnableThemeDialogTexture, 2);
-UNIMPL_FUNC(EnableTheming, 1);
-UNIMPL_FUNC(GetCurrentThemeName, 6);
-UNIMPL_FUNC(GetThemeAppProperties, 0);
-UNIMPL_FUNC(GetThemeBackgroundContentRect, 6);
-UNIMPL_FUNC(GetThemeBackgroundExtent, 6);
-UNIMPL_FUNC(GetThemeBackgroundRegion, 6);
-UNIMPL_FUNC(GetThemeBool, 5);
-UNIMPL_FUNC(GetThemeColor, 5);
-UNIMPL_FUNC(GetThemeDocumentationProperty, 4);
-UNIMPL_FUNC(GetThemeEnumValue, 5);
-UNIMPL_FUNC(GetThemeFilename, 6);
-UNIMPL_FUNC(GetThemeFont, 6);
-UNIMPL_FUNC(GetThemeInt, 5);
-UNIMPL_FUNC(GetThemeIntList, 5);
-UNIMPL_FUNC(GetThemeMargins, 7);
-UNIMPL_FUNC(GetThemeMetric, 6);
-UNIMPL_FUNC(GetThemePartSize, 7);
-UNIMPL_FUNC(GetThemePosition, 5);
-UNIMPL_FUNC(GetThemePropertyOrigin, 5);
-UNIMPL_FUNC(GetThemeRect, 5);
-UNIMPL_FUNC(GetThemeString, 6);
-UNIMPL_FUNC(GetThemeSysBool, 2);
-UNIMPL_FUNC(GetThemeSysColor, 2);
-UNIMPL_FUNC(GetThemeSysColorBrush, 2);
-UNIMPL_FUNC(GetThemeSysFont, 3);
-UNIMPL_FUNC(GetThemeSysInt, 3);
-UNIMPL_FUNC(GetThemeSysSize, 2);
-UNIMPL_FUNC(GetThemeSysString, 4);
-UNIMPL_FUNC(GetThemeTextExtent, 9);
-UNIMPL_FUNC(GetThemeTextMetrics, 5);
-UNIMPL_FUNC(GetWindowTheme, 1);
-UNIMPL_FUNC(HitTestThemeBackground, 10);
-UNIMPL_FUNC(IsAppThemed, 0);
-UNIMPL_FUNC(IsThemeActive, 0);
-UNIMPL_FUNC(IsThemeBackgroundPartiallyTransparent, 3);
-UNIMPL_FUNC(IsThemeDialogTextureEnabled, 1);
-UNIMPL_FUNC(IsThemePartDefined, 3);
-UNIMPL_FUNC(OpenThemeData, 2);
-UNIMPL_FUNC(SetThemeAppProperties, 1);
-UNIMPL_FUNC(SetWindowTheme, 3);
+UNIMPL_FUNC(CloseThemeData, 1, _E_NOTIMPL);
+UNIMPL_FUNC(DrawThemeBackground, 6, _E_NOTIMPL);
+UNIMPL_FUNC(DrawThemeBackgroundEx, 6, _E_NOTIMPL);
+UNIMPL_FUNC(DrawThemeEdge, 8, _E_NOTIMPL);
+UNIMPL_FUNC(DrawThemeIcon, 7, _E_NOTIMPL);
+UNIMPL_FUNC(DrawThemeParentBackground, 3, _E_NOTIMPL);
+UNIMPL_FUNC(DrawThemeText, 9, _E_NOTIMPL);
+UNIMPL_FUNC(EnableThemeDialogTexture, 2, _E_NOTIMPL);
+UNIMPL_FUNC(EnableTheming, 1, _E_NOTIMPL);
+UNIMPL_FUNC(GetCurrentThemeName, 6, _E_NOTIMPL);
+UNIMPL_FUNC(GetThemeAppProperties, 0, 0);
+UNIMPL_FUNC(GetThemeBackgroundContentRect, 6, _E_NOTIMPL);
+UNIMPL_FUNC(GetThemeBackgroundExtent, 6, _E_NOTIMPL);
+UNIMPL_FUNC(GetThemeBackgroundRegion, 6, _E_NOTIMPL);
+UNIMPL_FUNC(GetThemeBool, 5, _E_NOTIMPL);
+UNIMPL_FUNC(GetThemeColor, 5, _E_NOTIMPL);
+UNIMPL_FUNC(GetThemeDocumentationProperty, 4, _E_NOTIMPL);
+UNIMPL_FUNC(GetThemeEnumValue, 5, _E_NOTIMPL);
+UNIMPL_FUNC(GetThemeFilename, 6, _E_NOTIMPL);
+UNIMPL_FUNC(GetThemeFont, 6, _E_NOTIMPL);
+UNIMPL_FUNC(GetThemeInt, 5, _E_NOTIMPL);
+UNIMPL_FUNC(GetThemeIntList, 5, _E_NOTIMPL);
+UNIMPL_FUNC(GetThemeMargins, 7, _E_NOTIMPL);
+UNIMPL_FUNC(GetThemeMetric, 6, _E_NOTIMPL);
+UNIMPL_FUNC(GetThemePartSize, 7, _E_NOTIMPL);
+UNIMPL_FUNC(GetThemePosition, 5, _E_NOTIMPL);
+UNIMPL_FUNC(GetThemePropertyOrigin, 5, _E_NOTIMPL);
+UNIMPL_FUNC(GetThemeRect, 5, _E_NOTIMPL);
+UNIMPL_FUNC(GetThemeString, 6, _E_NOTIMPL);
+UNIMPL_FUNC(GetThemeTextExtent, 9, _E_NOTIMPL);
+UNIMPL_FUNC(GetThemeTextMetrics, 5, _E_NOTIMPL);
+UNIMPL_FUNC(GetWindowTheme, 1, 0);
+UNIMPL_FUNC(HitTestThemeBackground, 10, _E_NOTIMPL);
+UNIMPL_FUNC(IsAppThemed, 0, FALSE);
+UNIMPL_FUNC(IsThemeActive, 0, FALSE);
+UNIMPL_FUNC(IsThemeBackgroundPartiallyTransparent, 3, FALSE);
+UNIMPL_FUNC(IsThemeDialogTextureEnabled, 1, FALSE);
+UNIMPL_FUNC(IsThemePartDefined, 3, FALSE);
+UNIMPL_FUNC(OpenThemeData, 2, 0);
+UNIMPL_FUNC(SetThemeAppProperties, 1, 0);
+UNIMPL_FUNC(SetWindowTheme, 3, _E_NOTIMPL);
