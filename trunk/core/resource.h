90decde8af6784306abd9a762145d390327bf3cc
xeno86 <xeno86@3ad93e46-0ec0-498d-9331-a2525978a03b>
2009-11-04 00:31:42 +0000

    added option to enable/disable api logging feature on the fly

--- trunk/core/resource.h
+++ trunk/core/resource.h
@@ -14,13 +14,14 @@
 #define IDM_TOFILE                      40001
 #define IDM_CLEAR                       40002
 #define IDM_FILTER                      40003
+#define IDM_ENABLE                      40004
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        104
-#define _APS_NEXT_COMMAND_VALUE         40004
+#define _APS_NEXT_COMMAND_VALUE         40005
 #define _APS_NEXT_CONTROL_VALUE         1006
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
