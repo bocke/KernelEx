52b49f558cc6497f4063186086e28c92a3d6c1b4
xeno86 <xeno86@3ad93e46-0ec0-498d-9331-a2525978a03b>
2010-02-14 20:11:02 +0000

    implemented USP font cache

---
+++ trunk/apilibs/kexbasen/gdi32/ScriptCache.h
@@ -0,0 +1,59 @@
+/*
+ *  KernelEx
+ *  Copyright (C) 2010, Tihiy, Xeno86
+ *
+ *  This file is part of KernelEx source code.
+ *
+ *  KernelEx is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published
+ *  by the Free Software Foundation; version 2 of the License.
+ *
+ *  KernelEx is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with GNU Make; see the file COPYING.  If not, write to
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+
+#ifndef __SCRIPTCACHE_H
+#define __SCRIPTCACHE_H
+
+#include <windows.h>
+#include <usp10.h>
+#pragma warning(disable:4530) //we don't do exception handling
+#include <list>
+#pragma warning(default:4530)
+
+#define MAXSCRIPTCACHESIZE 10
+
+typedef struct
+{
+	HFONT hFont;
+	SCRIPT_CACHE cache;
+} FONTCACHE, *PFONTCACHE;
+
+using namespace std;
+
+class ScriptCache
+{
+public:
+		static ScriptCache instance;
+		~ScriptCache();
+		SCRIPT_CACHE GetCache(HFONT hFont);
+		void SetCache(HFONT hFont, SCRIPT_CACHE newcache);
+		void ResetCache(HFONT hFont);
+		void Lock();
+		void Unlock();
+
+private:
+		list<FONTCACHE> cache;
+		CRITICAL_SECTION cs;
+
+		ScriptCache();
+};
+
+#endif
