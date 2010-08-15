c2f0f7f9ebce0eec323eac2ffb688657834fab8b
xeno86 <xeno86@3ad93e46-0ec0-498d-9331-a2525978a03b>
2010-08-15 19:25:29 +0000

    added WINMM.waveInGetDevCapsW, waveOutGetDevCapsW (makes Flash Player 10.1 volume control work)

--- trunk/apilibs/kexbasen/main.c
+++ trunk/apilibs/kexbasen/main.c
@@ -31,9 +31,10 @@
 #include "rpcrt4/_rpcrt4_apilist.h"
 #include "winspool/_winspool_apilist.h"
 #include "shfolder/_shfolder_apilist.h"
+#include "winmm/_winmm_apilist.h"
 //#include "/__apilist.h"
 
-static apilib_api_table api_table[10];
+static apilib_api_table api_table[11];
 
 static void fill_apitable()
 {
@@ -46,6 +47,7 @@ static void fill_apitable()
 	api_table[6] = apitable_rpcrt4;
 	api_table[7] = apitable_winspool;
 	api_table[8] = apitable_shfolder;
+	api_table[9] = apitable_winmm;
 	//last entry is null terminator
 }
 
@@ -59,7 +61,9 @@ const apilib_api_table* get_api_table()
 
 static BOOL init()
 {
-	return common_init() && init_kernel32() && init_gdi32() && init_user32() && init_advapi32() && init_comdlg32() && init_shell32() && init_rpcrt4() && init_winspool() && init_shfolder();
+	return common_init() && init_kernel32() && init_gdi32() && init_user32() 
+		&& init_advapi32() && init_comdlg32() && init_shell32() && init_rpcrt4() 
+		&& init_winspool() && init_shfolder() && init_winmm();
 }
 
 static void uninit()
