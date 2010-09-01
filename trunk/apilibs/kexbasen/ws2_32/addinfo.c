069ca2425350c34415793fa13f2d5db36eefec15
xeno86 <xeno86@3ad93e46-0ec0-498d-9331-a2525978a03b>
2010-09-01 21:42:50 +0000

    quick fix for PSDK > feb 2003 - second attempt

--- trunk/apilibs/kexbasen/ws2_32/addinfo.c
+++ trunk/apilibs/kexbasen/ws2_32/addinfo.c
@@ -22,8 +22,8 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <winsock2.h>
-#include <ws2tcpip.h>
 #define _WSPIAPI_COUNTOF(_Array) (sizeof(_Array) / sizeof(_Array[0]))
+#include <ws2tcpip.h>
 #include <wspiapi.h>
 
 /* MAKE_EXPORT getaddrinfo_new=getaddrinfo */
