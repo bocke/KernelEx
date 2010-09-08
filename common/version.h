556af032cd4de64a2cfc5af00e81485d0cef13a3
xeno86 <xeno86@3ad93e46-0ec0-498d-9331-a2525978a03b>
2010-09-08 20:57:38 +0000

    updated version numbers for 4.5 RC 2 release updated changelog and news

--- common/version.h
+++ common/version.h
@@ -22,9 +22,9 @@
 #ifndef __VERSION_H
 #define __VERSION_H
 
-#define VERSION_STR      "4.5 RC 1"
-#define VERSION_CODE     0x0405000B
-#define RCVERSION        4, 5, 1, 1
-#define _RCVERSION_      "4, 5, 1, 1"
+#define VERSION_STR      "4.5 RC 2"
+#define VERSION_CODE     0x0405000C
+#define RCVERSION        4, 5, 1, 2
+#define _RCVERSION_      "4, 5, 1, 2"
 
 #endif
