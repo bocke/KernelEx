8498ae5929b85d07bb9e2344a55a9e63620ea4bb
xeno86 <xeno86@3ad93e46-0ec0-498d-9331-a2525978a03b>
2010-12-05 22:30:08 +0000

    whitespace fixes

--- trunk/util/sdbcreate/sdbcreate.cpp
+++ trunk/util/sdbcreate/sdbcreate.cpp
@@ -39,226 +39,226 @@ static map<wstring, TAGID> tagidmap;
 
 void fail()
 {
-    SdbCloseDatabase(pdb);
-    DeleteFileW(sdbfile);
-    exit(-1);
+	SdbCloseDatabase(pdb);
+	DeleteFileW(sdbfile);
+	exit(-1);
 }
 
 void WriteTransform(LPWSTR section)
 {
-    //query file name
-    WCHAR filebuf[MAX_PATH];
-    WCHAR filepath[MAX_PATH];
-    WCHAR name[MAX_PATH];
+	//query file name
+	WCHAR filebuf[MAX_PATH];
+	WCHAR filepath[MAX_PATH];
+	WCHAR name[MAX_PATH];
 
-    if (!GetPrivateProfileStringW(section, L"Transform", NULL, filebuf, MAX_PATH, inifile))
+	if (!GetPrivateProfileStringW(section, L"Transform", NULL, filebuf, MAX_PATH, inifile))
 	{
 		printf("Section %ls does not specify file!\n", section);
 		fail();
-    }
-    
+	}
+	
 	//build path to file relative to ini
-    lstrcpyW(filepath, inifile);
-    PathRemoveFileSpecW(filepath);
-    PathAppendW(filepath, filebuf);
-    
+	lstrcpyW(filepath, inifile);
+	PathRemoveFileSpecW(filepath);
+	PathAppendW(filepath, filebuf);
+	
 	LPWSTR filename = PathFindFileNameW(filepath);
 
-    lstrcpyW(name, filepath);
-    PathRemoveExtensionW(name);
-    PathStripPathW(name);
+	lstrcpyW(name, filepath);
+	PathRemoveExtensionW(name);
+	PathStripPathW(name);
 
-    if (tagidmap.count(name) > 0)
+	if (tagidmap.count(name) > 0)
 		return;			//already have it
 
-    printf("Adding %ls...", filepath);
+	printf("Adding %ls...", filepath);
 
-    //write FILE
-    TAGID filetagid = SdbBeginWriteListTag(pdb, TAG_FILE);
-    SdbWriteStringTag(pdb, TAG_NAME, filename);
-    if (SdbWriteBinaryTagFromFile(pdb, TAG_FILE_BITS, filepath))
+	//write FILE
+	TAGID filetagid = SdbBeginWriteListTag(pdb, TAG_FILE);
+	SdbWriteStringTag(pdb, TAG_NAME, filename);
+	if (SdbWriteBinaryTagFromFile(pdb, TAG_FILE_BITS, filepath))
 		printf("Success!\n");
-    else
+	else
 	{
 		printf("Failure!\n");
 		fail();
-    }
-    SdbEndWriteListTag(pdb, filetagid);
+	}
+	SdbEndWriteListTag(pdb, filetagid);
 
-    //write MSI_TRANSFORM
-    TAGID transfid = SdbBeginWriteListTag(pdb, TAG_MSI_TRANSFORM);
-    SdbWriteStringTag(pdb, TAG_NAME, name);
-    SdbWriteStringTag(pdb, TAG_MSI_TRANSFORM_FILE, filename);
-    SdbWriteDWORDTag(pdb, TAG_MSI_TRANSFORM_TAGID, filetagid);
-    SdbEndWriteListTag(pdb, transfid);
+	//write MSI_TRANSFORM
+	TAGID transfid = SdbBeginWriteListTag(pdb, TAG_MSI_TRANSFORM);
+	SdbWriteStringTag(pdb, TAG_NAME, name);
+	SdbWriteStringTag(pdb, TAG_MSI_TRANSFORM_FILE, filename);
+	SdbWriteDWORDTag(pdb, TAG_MSI_TRANSFORM_TAGID, filetagid);
+	SdbEndWriteListTag(pdb, transfid);
 
-    //add transform tagid to name - tagid map
+	//add transform tagid to name - tagid map
 	tagidmap[name] = transfid;
 }
 
 void WritePackage(LPWSTR section)
 {
-    //query file name
-    WCHAR filebuf[MAX_PATH];
-    WCHAR filepath[MAX_PATH];
-    WCHAR name[MAX_PATH];
+	//query file name
+	WCHAR filebuf[MAX_PATH];
+	WCHAR filepath[MAX_PATH];
+	WCHAR name[MAX_PATH];
 
-    GetPrivateProfileStringW(section, L"Transform", NULL, filebuf, MAX_PATH, inifile);
+	GetPrivateProfileStringW(section, L"Transform", NULL, filebuf, MAX_PATH, inifile);
 
-    //build path to file relative to ini
-    lstrcpyW(filepath, inifile);
-    PathRemoveFileSpecW(filepath);
-    PathAppendW(filepath, filebuf);
+	//build path to file relative to ini
+	lstrcpyW(filepath, inifile);
+	PathRemoveFileSpecW(filepath);
+	PathAppendW(filepath, filebuf);
 
-    LPWSTR filename = PathFindFileNameW(filepath);
+	LPWSTR filename = PathFindFileNameW(filepath);
 
-    lstrcpyW(name, filepath);
-    PathRemoveExtensionW(name);
-    PathStripPathW(name);
+	lstrcpyW(name, filepath);
+	PathRemoveExtensionW(name);
+	PathStripPathW(name);
 
-    TAGID transfid = tagidmap[name];
+	TAGID transfid = tagidmap[name];
 
-    //query attributes
-    WCHAR appname[200];
-    WCHAR packagecode[100];
-    GUID guid;
+	//query attributes
+	WCHAR appname[200];
+	WCHAR packagecode[100];
+	GUID guid;
 
-    GetPrivateProfileStringW(section, L"AppName", section, appname, 200, inifile);
-    GetPrivateProfileStringW(section, L"ProductCode", NULL, packagecode, 100, inifile);
-    if (!SdbGUIDFromString(packagecode, &guid))
+	GetPrivateProfileStringW(section, L"AppName", section, appname, 200, inifile);
+	GetPrivateProfileStringW(section, L"ProductCode", NULL, packagecode, 100, inifile);
+	if (!SdbGUIDFromString(packagecode, &guid))
 	{
 		printf("Section %ls specifies invalid ProductCode!\n", section);
 		fail();
-    }
+	}
 
-    printf("Writing section %ls\n", section);
+	printf("Writing section %ls\n", section);
 
-    //write MSI_PACKAGE
-    TAGID tagid = SdbBeginWriteListTag(pdb, TAG_MSI_PACKAGE);
-    SdbWriteStringTag(pdb, TAG_NAME, section);
-    SdbWriteStringTag(pdb, TAG_APP_NAME, appname);
-    SdbWriteBinaryTag(pdb, TAG_MSI_PACKAGE_ID,(PBYTE) & guid, sizeof(GUID));
+	//write MSI_PACKAGE
+	TAGID tagid = SdbBeginWriteListTag(pdb, TAG_MSI_PACKAGE);
+	SdbWriteStringTag(pdb, TAG_NAME, section);
+	SdbWriteStringTag(pdb, TAG_APP_NAME, appname);
+	SdbWriteBinaryTag(pdb, TAG_MSI_PACKAGE_ID,(PBYTE) & guid, sizeof(GUID));
 
-    //write MSI_TRANSFORM_REF
-    TAGID reftagid = SdbBeginWriteListTag(pdb, TAG_MSI_TRANSFORM_REF);
-    SdbWriteStringTag(pdb, TAG_NAME, name);
-    SdbWriteDWORDTag(pdb, TAG_MSI_TRANSFORM_TAGID, transfid);
-    SdbEndWriteListTag(pdb, reftagid);
-    SdbEndWriteListTag(pdb, tagid);
+	//write MSI_TRANSFORM_REF
+	TAGID reftagid = SdbBeginWriteListTag(pdb, TAG_MSI_TRANSFORM_REF);
+	SdbWriteStringTag(pdb, TAG_NAME, name);
+	SdbWriteDWORDTag(pdb, TAG_MSI_TRANSFORM_TAGID, transfid);
+	SdbEndWriteListTag(pdb, reftagid);
+	SdbEndWriteListTag(pdb, tagid);
 
-    //write registry
-    if (regfile)
+	//write registry
+	if (regfile)
 	{
 		fprintf(regfile, "[HKEY_LOCAL_MACHINE\\Software\\Microsoft\\Windows NT\\"
 				"CurrentVersion\\AppCompatFlags\\Custom\\%ls]\n", packagecode);
 		fprintf(regfile, "\"%ls\"=\"%ls\"\n", PathFindFileNameW(sdbfile), appname);
 		fprintf(regfile, "\n");
-    }
+	}
 }
 
 
 void ForEachSection(STRPROC proc)
 {
-    WCHAR sections[0x10000] = { 0 };
-    LPWSTR strW = sections;
+	WCHAR sections[0x10000] = { 0 };
+	LPWSTR strW = sections;
 
-    GetPrivateProfileSectionNamesW(strW, 0x10000, inifile);
-    for(; *strW; strW += lstrlenW(strW) + 1)
+	GetPrivateProfileSectionNamesW(strW, 0x10000, inifile);
+	for(; *strW; strW += lstrlenW(strW) + 1)
 		proc(strW);
 }
 
 void CountSection(LPWSTR section)
 {
-    sections++;
+	sections++;
 }
 
 void WriteTransforms()
 {
-    TAGID tagid = SdbBeginWriteListTag(pdb, TAG_LIBRARY);
-    ForEachSection(WriteTransform);
-    SdbEndWriteListTag(pdb, tagid);
+	TAGID tagid = SdbBeginWriteListTag(pdb, TAG_LIBRARY);
+	ForEachSection(WriteTransform);
+	SdbEndWriteListTag(pdb, tagid);
 }
 
 void WritePackages()
 {
-    ForEachSection(WritePackage);
+	ForEachSection(WritePackage);
 }
 
 void WriteDatabase()
 {
-    //count number of sections
-    sections = 0;
-    ForEachSection(CountSection);
-    sections = (sections + 15) & ~15;
+	//count number of sections
+	sections = 0;
+	ForEachSection(CountSection);
+	sections = (sections + 15) & ~15;
  
 	//create indexes
-    INDEXID idx1;
-    INDEXID idx2;
-    INDEXID idx3;
-    SdbDeclareIndex(pdb, TAG_MSI_TRANSFORM, TAG_NAME, sections, TRUE, &idx1);
-    SdbDeclareIndex(pdb, TAG_MSI_PACKAGE, TAG_MSI_PACKAGE_ID, sections, FALSE, &idx2);
-    SdbDeclareIndex(pdb, TAG_MSI_PACKAGE, TAG_EXE_ID, sections, TRUE, &idx3);
-    SdbCommitIndexes(pdb);
+	INDEXID idx1;
+	INDEXID idx2;
+	INDEXID idx3;
+	SdbDeclareIndex(pdb, TAG_MSI_TRANSFORM, TAG_NAME, sections, TRUE, &idx1);
+	SdbDeclareIndex(pdb, TAG_MSI_PACKAGE, TAG_MSI_PACKAGE_ID, sections, FALSE, &idx2);
+	SdbDeclareIndex(pdb, TAG_MSI_PACKAGE, TAG_EXE_ID, sections, TRUE, &idx3);
+	SdbCommitIndexes(pdb);
 
-    //enable indexing
-    SdbStartIndexing(pdb, idx1);
-    SdbStartIndexing(pdb, idx2);
-    SdbStartIndexing(pdb, idx3);
-    
+	//enable indexing
+	SdbStartIndexing(pdb, idx1);
+	SdbStartIndexing(pdb, idx2);
+	SdbStartIndexing(pdb, idx3);
+	
 	//create database
-    TAGID tagid = SdbBeginWriteListTag(pdb, TAG_DATABASE);
-    WriteTransforms();
-    WritePackages();
-    SdbEndWriteListTag(pdb, tagid);
+	TAGID tagid = SdbBeginWriteListTag(pdb, TAG_DATABASE);
+	WriteTransforms();
+	WritePackages();
+	SdbEndWriteListTag(pdb, tagid);
 
-    //disable indexing
-    SdbStopIndexing(pdb, idx1);
-    SdbStopIndexing(pdb, idx2);
-    SdbStopIndexing(pdb, idx3);
+	//disable indexing
+	SdbStopIndexing(pdb, idx1);
+	SdbStopIndexing(pdb, idx2);
+	SdbStopIndexing(pdb, idx3);
 }
 
 int wmain(int argc, WCHAR * argv[])
 {
-    if (argc != 3 && argc != 4)
+	if (argc != 3 && argc != 4)
 	{
 		printf("Usage: sdbcreate.exe dbspec.ini out.sdb [out.reg]\n");
 		return 1;
-    }
+	}
 
-    //check ini
-    GetFullPathNameW(argv[1], MAX_PATH, inifile, NULL);
-    if (GetFileAttributesW(inifile) == INVALID_FILE_ATTRIBUTES)
+	//check ini
+	GetFullPathNameW(argv[1], MAX_PATH, inifile, NULL);
+	if (GetFileAttributesW(inifile) == INVALID_FILE_ATTRIBUTES)
 	{
 		printf("Failed to open %ls!\n", inifile);
 		return 2;
-    }
+	}
 
-    //create sdb
-    sdbfile = argv[2];
-    printf("Creating %ls...\n", sdbfile);
-    pdb = SdbCreateDatabase(sdbfile, DOS_PATH);
-    if (!pdb)
+	//create sdb
+	sdbfile = argv[2];
+	printf("Creating %ls...\n", sdbfile);
+	pdb = SdbCreateDatabase(sdbfile, DOS_PATH);
+	if (!pdb)
 	{
 		printf("Failed to create database!\n");
 		return 3;
-    }
+	}
 
-    //create reg
-    regfile = NULL;
-    if (argc == 4)
+	//create reg
+	regfile = NULL;
+	if (argc == 4)
 	{
 		regfile = _wfopen(argv[3], L"w");
 		fprintf(regfile, "REGEDIT4\n\n");
-    }
+	}
 
-    //write
-    WriteDatabase();
+	//write
+	WriteDatabase();
 
-    //close
-    SdbCloseDatabaseWrite(pdb);
-    if (regfile)
+	//close
+	SdbCloseDatabaseWrite(pdb);
+	if (regfile)
 		fclose(regfile);
 
-    printf("Finished.");
-    return 0;
+	printf("Finished.");
+	return 0;
 }
