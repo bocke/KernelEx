9401653747e155a53cc0f4285f18653396af37fa
xeno86 <xeno86@3ad93e46-0ec0-498d-9331-a2525978a03b>
2010-12-05 19:26:53 +0000

    cryptui: added missing declarations

--- trunk/apilibs/kexbasen/cryptui/CryptUIDlgViewContext.c
+++ trunk/apilibs/kexbasen/cryptui/CryptUIDlgViewContext.c
@@ -20,29 +20,56 @@
  */
 
 #include <windows.h>
-#include <cryptuiapi.h>
+#include <wintrust.h>
 
 typedef struct {
-  DWORD            dwSize;
-  HWND             hwndParent;
-  DWORD            dwFlags;
-  LPCWSTR          szTitle;
-  PCCTL_CONTEXT    pCtlContext;
-  DWORD			   unknown[6];
+	DWORD            dwSize;
+	HWND             hwndParent;
+	DWORD            dwFlags;
+	LPCWSTR          szTitle;
+	PCCTL_CONTEXT    pCtlContext;
+	DWORD            unknown[6];
 } CRYPTUI_VIEWCTL_STRUCTW; //size = 0x2C
 
 typedef struct {
-  DWORD            dwSize;
-  HWND             hwndParent;
-  DWORD            dwFlags;
-  LPCWSTR          szTitle;
-  PCCRL_CONTEXT    pCtlContext;
-  DWORD			   unknown[4];
+	DWORD            dwSize;
+	HWND             hwndParent;
+	DWORD            dwFlags;
+	LPCWSTR          szTitle;
+	PCCRL_CONTEXT    pCtlContext;
+	DWORD            unknown[4];
 } CRYPTUI_VIEWCRL_STRUCTW; //size = 0x24
+
+typedef struct tagCRYPTUI_VIEWCERTIFICATE_STRUCTW {
+    DWORD            dwSize;
+    HWND             hwndParent;
+    DWORD            dwFlags;
+    LPCWSTR          szTitle;
+    PCCERT_CONTEXT   pCertContext;
+    LPCSTR*          rgszPurposes;
+    DWORD            cPurposes;
+    union
+    {
+		CRYPT_PROVIDER_DATA const* pCryptProviderData;
+		HANDLE                     hWVTStateData;
+    };
+    BOOL             fpCryptProviderDataTrustedUsage;
+    DWORD            idxSigner;
+    DWORD            idxCert;
+    BOOL             fCounterSigner;
+    DWORD            idxCounterSigner;
+    DWORD            cStores;
+    HCERTSTORE*      rghStores;
+    DWORD            cPropSheetPages;
+    LPCPROPSHEETPAGEW rgPropSheetPages;
+    DWORD            nStartPage;
+} CRYPTUI_VIEWCERTIFICATE_STRUCTW,*PCRYPTUI_VIEWCERTIFICATE_STRUCTW;
+typedef const CRYPTUI_VIEWCERTIFICATE_STRUCTW *PCCRYPTUI_VIEWCERTIFICATE_STRUCTW;
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+BOOL WINAPI CryptUIDlgViewCertificateW(PCCRYPTUI_VIEWCERTIFICATE_STRUCTW, BOOL*);
 BOOL WINAPI CryptUIDlgViewCTLW(CRYPTUI_VIEWCTL_STRUCTW*);
 BOOL WINAPI CryptUIDlgViewCRLW(CRYPTUI_VIEWCRL_STRUCTW*);
 #ifdef __cplusplus
