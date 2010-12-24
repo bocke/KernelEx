a09881ec1cb7c044278b681f503c2fe168601fc3
xeno86 <xeno86@3ad93e46-0ec0-498d-9331-a2525978a03b>
2010-12-24 13:16:41 +0000

    verifier: new welcome screen

--- trunk/verify/resource.h
+++ trunk/verify/resource.h
@@ -2,17 +2,21 @@
 // Microsoft Developer Studio generated include file.
 // Used by verify.rc
 //
-#define IDS_SUCCESS                     1
-#define IDS_FAILURE                     2
-#define IDS_TITLE                       3
+#define IDS_FAILURE                     1
+#define IDS_FAILURETITLE                2
+#define IDD_DIALOG1                     101
+#define IDD_DIALOG2                     102
+#define IDC_CHECK1                      1000
+#define IDC_LINK                        1001
+#define IDC_VERSION                     1003
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_RESOURCE_VALUE        104
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_CONTROL_VALUE         1004
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
